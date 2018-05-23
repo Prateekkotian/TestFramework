87de155f9f40fbf820736d30946124cea4b027b9
Prateek Kumar <prateek.kumar@sourcebits.com>
2018-05-23 14:48:36 +0530

    Added framework code

---
+++ TestFramework/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  TestFramework
+//
+//  Created by Prateek Kumar on 23/05/18.
+//  Copyright © 2018 Prateek Kumar. All rights reserved.
+//
+
+import UIKit
+
+open class TestClass: NSObject {
+    static let shared = TestClass()
+    func foo() {}
+}
