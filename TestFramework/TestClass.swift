927e9db8c296d982f96519c409212faf8c496436
Prateek Kumar <prateek.kumar@sourcebits.com>
2018-08-02 12:41:09 +0530

    added fb login code

--- TestFramework/TestClass.swift
+++ TestFramework/TestClass.swift
@@ -7,8 +7,25 @@
 //
 
 import UIKit
+import FacebookLogin
 
 open class TestClass: NSObject {
     static let shared = TestClass()
     func foo() {}
+
+    public func facebookLogin() {
+        let loginManager = LoginManager()
+        loginManager.logOut()
+        loginManager.logIn(readPermissions: [.publicProfile, .email], viewController: nil) { (loginResult) in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("user cancelled the login")
+            case .success(_, _, let accessToken): // (let grantedPermissions, let declinedPermissions, let accessToken)
+                print("Logged in with FB ID: \(String(describing: accessToken.userId))")
+                //self?.getFBAccountDetails()
+            }
+        }
+    }
 }
